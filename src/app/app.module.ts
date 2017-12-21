7470d8d9c23464fa798245af2223b5223c9efe5f
Jackson <jackson.palacios@correounivalle.edu.co>
2017-12-21 01:25:43 -0500

    Se crea el componente search.component.ts y el servicio search.service.ts, para continuar su desarrollo posteriormente

---
+++ src/app/app.module.ts
@@ -0,0 +1,67 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {
+  MatCheckbox,
+  MatFormField,
+  MatIconModule,
+  MatCardModule,
+  MatListModule,
+  MatInputModule,
+  MatDialogModule,
+  MatSelectModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatCheckboxModule,
+  MatGridListModule,
+  MatFormFieldModule,
+  MatSlideToggleModule,
+  MatProgressSpinnerModule,
+  MatRadioModule
+} from '@angular/material';
+import 'hammerjs';
+
+import { AppComponent } from './app.component';
+import { Pipe } from '@angular/core/src/metadata/directives';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { SearchComponent } from './search/search.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchComponent 
+  ],
+  imports: [
+    HttpModule,
+    FormsModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    BrowserModule,
+    MatInputModule,
+    MatDialogModule,    
+    MatSelectModule,
+    MatButtonModule,
+    AppRoutingModule,   
+    MatToolbarModule,
+    MatGridListModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatSlideToggleModule,
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule,
+    MatRadioModule
+
+  ],
+  providers: [],  
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
